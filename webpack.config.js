c2d9a1b62c416bcafd214ea32f727852d1d3aa15
tatsuma makino <devel.mckee.1531@gmail.com>
2021-10-20 12:55:41 +0900

    add babel preset and react

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
             plugins: ['@babel/plugin-transform-runtime'],
           },
         },
