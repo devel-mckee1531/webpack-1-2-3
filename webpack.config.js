954e1ad72bca1b72d88d257f6d9cfcc421f35bf6
tatsuma makino <devel.mckee.1531@gmail.com>
2021-10-20 11:45:39 +0900

    add scss transpile settings

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,20 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 2,
+            },
+          },
+          'sass-loader',
+        ],
+      },
     ],
   },
 }
